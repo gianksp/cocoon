a9da7cf058873aececb06160b09052a5476efda9
Giancarlo Sanchez <giancarlosanchez@Giancarlos-MacBook-Pro.local>
2015-02-26 22:33:05 +0000

    Unit testing support with Karma and Jasmine

---
+++ www/module/core/appController.js
@@ -0,0 +1,12 @@
+/**
+ * Main application controller
+ * @param  {[type]} $rootScope  [description]
+ * @param  {[type]} $scope      [description]
+ * @param  {[type]} $ionicModal [description]
+ * @param  {[type]} $timeout)   {}          [description]
+ * @return {[type]}             [description]
+ */
+angular.module('cocoon').controller('appController', function($rootScope, $scope, $ionicModal, $timeout) {
+
+
+});
---
+++ www/test/core/appController.js
@@ -0,0 +1,10 @@
+'use strict';
+  
+describe('Sample', function(){
+    var scope; //we'll use this scope in our tests
+  
+    // tests start here
+    it('should have variable text = "Hello World!"', function(){
+        expect('Sup').toBe('Sup');
+    });
+});
