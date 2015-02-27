02b6a474f60e73b3b23863354bee2cacac6c1497
Giancarlo Sanchez <giancarlosanchez@Giancarlos-MacBook-Pro.local>
2015-02-27 09:38:32 +0000

    Restructure folder

--- www/module/core/core.controller.js
+++ www/module/core/core.controller.js
@@ -8,5 +8,32 @@
  */
 angular.module('core').controller('controller', function($rootScope, $scope, $ionicModal, $timeout) {
 
+    // Default to an empty list of friends - this data
+    // will be loaded from the remote data module using
+    // require.
+    $scope.friends = [];
+
+
+    // ---
+    // PUBLIC METHODS.
+    // ---
+
+
+    // I load the remote friends data.
+    $scope.loadData = function() {
+
+        require(
+            [ "https://code.jquery.com/jquery-2.1.3.min.js" ],
+            function( newFriends ) {
+
+                $scope.friends = newFriends;
+
+            }
+        );
+
+    };
+
+
+    $scope.loadData();
 
 });
